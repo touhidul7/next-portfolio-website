8d21572fad3aba7db1078007d0464a711ef62844
touhidul-islam-html <ti360109@gmail.com>
2025-01-21 03:34:55 +0600

    update

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,13 +1,10 @@
+'use client'
 import Image from "next/image";
 import React from "react";
 import Heading from "../component/Heading";
-import brittolearningimg from "@/public/Images/projectimg/brittolearning.jpg";
-import brittosoftimg from "@/public/Images/projectimg/brittosoft.jpg";
-import brittowalletimg from "@/public/Images/projectimg/brittowallet.jpg";
-import smsimg from "@/public/Images/projectimg/sms.jpg";
-import bistritiimg from "@/public/Images/projectimg/bistriti.jpg";
-import andaazimg from "@/public/Images/projectimg/andaaz.jpg";
+import projects from "@/public/data/projects.json";
 import Link from "next/link";
+
 
 export default function page() {
   return (
@@ -16,42 +13,15 @@ export default function page() {
         <Heading title="projects" />
       </div>
       <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
-        <Projectcard
-          title={"LMS Platform"}
-          technologies={"WordPress, Tutor LMS, Elementor"}
-          projectlink={"https://brittolearning.com/"}
-          image={brittolearningimg}
-        />
-        <Projectcard
-          title={"SMS Platform"}
-          technologies={"PHP, Laravel, MySQL, Bootstrap"}
-          image={smsimg}
-          projectlink={""}
-        />
-        <Projectcard
-          title={"Interior Design Portfolio"}
-          technologies={"React, Next JS, Tailwind CSS"}
-          image={bistritiimg}
-          projectlink={"https://bistriti.vercel.app/"}
-        />
-        <Projectcard
-          title={"Personal Budget App"}
-          technologies={"React, Redux,Tailwind CSS"}
-          image={brittowalletimg}
-          projectlink={"https://drive.google.com/file/d/13oE6gWCup-tsJYjJfOK2gWOAEyimC_G3/view?usp=drive_link"}
-        />
-        <Projectcard
-          title={"Development Company"}
-          technologies={"React , Next JS, Tailwind CSS"}
-          image={brittosoftimg}
-          projectlink={"https://brittosoft.com/"}
-        />
-        <Projectcard
-          title={"E-commerce Website"}
-          technologies={"WordPress, Woocommerce, Elementor"}
-          image={andaazimg}
-          projectlink={"https://andaazbd.com/"}
-        />
+         {projects?.map((project, index) => (
+          <Projectcard
+            key={index}
+            title={project.title}
+            technologies={project.technologies}
+            projectlink={project.link}
+            image={project.image}
+          />
+        ))} 
 </div>
 
     </div>
