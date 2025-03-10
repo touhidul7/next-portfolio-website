8db4ba2041a06085a5364f4e2e1556cf0d6f932f
touhidul-islam-html <ti360109@gmail.com>
2025-03-10 12:06:17 +0600

    update

--- app/component/Sidebar.js
+++ app/component/Sidebar.js
@@ -1,92 +1,94 @@
 'use client'
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import profileimg from "@/public/Images/Touhidul.jpeg";
 import Link from "next/link";
 import { AiOutlineMail } from "react-icons/ai";
 import { TbBrandFiverr } from "react-icons/tb";
 import { FaPhoneAlt, FaGithub } from "react-icons/fa";
 
-const Sidebar = ({className}) => {
+const Sidebar = ({ className }) => {
 
-/*   const serverapi = "https://mernstackportfoliobackend-teds3zq3.b4a.run/info";
-  const [info, setInfo] = useState(null); 
-  const [loading, setLoading] = useState(true);  */
+  const serverapi = "https://mernstackportfoliobackend-teds3zq3.b4a.run/info";
+  const [info, setInfo] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   // Fetch API data
-/*   useEffect(() => {
-    const fetchInfo = async () => {
-      try {
-        const response = await axios.get(serverapi);
-        setInfo(response.data[0]); 
+  useEffect(() => {
+    fetch(serverapi)
+      .then((response) => response.json())
+      .then((data) => {
+        setInfo(data[0]);
+        console.log(data);
+        
         setLoading(false);
-      } catch (error) {
+      })
+      .catch((error) => {
+        console.error("Error:", error);
         setLoading(false);
-        console.error("Error fetching info:", error);
-      }
-    };
+      });
+  }, []);
 
-    fetchInfo();
-  }, []); */
-
-  /* if (loading) {
-    return(<div>Loading....</div>)
+  if (loading) {
+    return (<div>Loading....</div>)
   }
 
-  if (!info) {
-    return(<div>Error Fetching Data</div>)
-  }
-   */
+   if (!info) {
+    return (<div>Error Fetching Data</div>)
+  } 
 
-// console.log(info);
+
+  console.log(info);
 
   return (
-   <>
-    <div className={`lg:w-[25%] border-[1px] border-[#282829] p-4 bg-[#1E1E1F] flex flex-col justify-start items-center gap-2 rounded-xl ${className}`}>
-      <div className="profile-img">
-        <Image
-          className="rounded-full"
-          width={150}
-          height={150}
-          src={profileimg}
-          alt="Touhidul"
-        />
+    <>
+      {/* <div className={`lg:w-[25%] border-[1px] border-[#282829] p-4 bg-[#1E1E1F] flex flex-col justify-start items-center gap-2 rounded-xl ${className}`}>
+        <div className="profile-img">
+          <Image
+            className="rounded-full"
+            width={150}
+            height={150}
+            src={profileimg}
+            alt="Touhidul"
+          />
+        </div>
+        <div className="name text-xl font-bold">Touhidul Islam</div>
+        <div className="role text-sm bg-[#2B2B2C] px-3 py-1 rounded-md">
+          Front-End Web Developer
+        </div>
+        <div className="devider w-full bg-[#282829] h-[2px] mt-2"></div>
+        <div className="data space-y-4 text-left">
+          <Iconbox title="Email" icon={<AiOutlineMail size={20} />} link={`mailto:touhiduliet@gmail.com`} content={"touhiduliet@gmail.com"} />
+          <Iconbox title="Phone" icon={<FaPhoneAlt size={20} />} link={`tel:+8801518999578`} content="+8801518999578" />
+          <Iconbox title="Github" icon={<FaGithub size={20} />} link="https://github.com/touhidul7" content="touhidul7" />
+          <Iconbox title="Fiverr" icon={<TbBrandFiverr size={20} />} link="https://fiverr.com/mdislam124" content="fiverr.com" />
+        </div>
+      </div> */}
+
+      <div className={`lg:w-[25%] border-[1px] border-[#282829] p-4 bg-[#1E1E1F] flex flex-col justify-start items-center gap-2 rounded-xl ${className}`}>
+        <div className="profile-img">
+          <Image
+            className="rounded-full"
+            width={150}
+            height={150}
+            src={profileimg}
+            alt="Touhidul"
+          />
+        </div>
+        <div className="name text-xl font-bold">{info.name}</div>
+        <div className="role text-sm bg-[#2B2B2C] px-3 py-1 rounded-md">
+          {info.JobTitle}
+        </div>
+        <div className="devider w-full bg-[#282829] h-[2px] mt-2"></div>
+        <div className="data space-y-4 text-left">
+          <Iconbox title="Email" icon={<AiOutlineMail size={20} />} link={`mailto:${info.email}`} content={info.email} />
+          <Iconbox title="Phone" icon={<FaPhoneAlt size={20} />} link={`tel:${info.phone}`} content={info.phone} />
+          <Iconbox title="Github" icon={<FaGithub size={20} />} link="https://github.com/touhidul7" content="touhidul7" />
+          <Iconbox title="Fiverr" icon={<TbBrandFiverr size={20} />} link="https://fiverr.com/mdislam124" content="fiverr.com" />
+        </div>
+      
       </div>
-      <div className="name text-xl font-bold">Touhidul Islam</div>
-      <div className="role text-sm bg-[#2B2B2C] px-3 py-1 rounded-md">
-      Front-End Web Developer
-      </div>
-      <div className="devider w-full bg-[#282829] h-[2px] mt-2"></div>
-      <div className="data space-y-4 text-left">
-        <Iconbox title="Email" icon={<AiOutlineMail size={20} />} link={`mailto:touhiduliet@gmail.com`} content={"touhiduliet@gmail.com"} />
-        <Iconbox title="Phone" icon={<FaPhoneAlt size={20} />} link={`tel:+8801518999578`} content="+8801518999578" />
-        <Iconbox title="Github" icon={<FaGithub size={20} />} link="https://github.com/touhidul7" content="touhidul7" />
-        <Iconbox title="Fiverr" icon={<TbBrandFiverr size={20} />} link="https://fiverr.com/mdislam124" content="fiverr.com" />
-      </div>
-    </div>
-    {/* <div className={`lg:w-[25%] border-[1px] border-[#282829] p-4 bg-[#1E1E1F] flex flex-col justify-start items-center gap-2 rounded-xl ${className}`}>
-      <div className="profile-img">
-        <Image
-          className="rounded-full"
-          width={150}
-          height={150}
-          src={profileimg}
-          alt="Touhidul"
-        />
-      </div>
-      <div className="name text-xl font-bold">{info.name}</div>
-      <div className="role text-sm bg-[#2B2B2C] px-3 py-1 rounded-md">
-      {info.JobTitle}
-      </div>
-      <div className="devider w-full bg-[#282829] h-[2px] mt-2"></div>
-      <div className="data space-y-4 text-left">
-        <Iconbox title="Email" icon={<AiOutlineMail size={20} />} link={`mailto:${info.email}`} content={info.email} />
-        <Iconbox title="Phone" icon={<FaPhoneAlt size={20} />} link={`tel:${info.phone}`} content={info.phone} />
-        <Iconbox title="Github" icon={<FaGithub size={20} />} link="https://github.com/touhidul7" content="touhidul7" />
-        <Iconbox title="Fiverr" icon={<TbBrandFiverr size={20} />} link="https://fiverr.com/mdislam124" content="fiverr.com" />
-      </div>
-    </div> */}
-   </>
+    </>
   );
 };
 
